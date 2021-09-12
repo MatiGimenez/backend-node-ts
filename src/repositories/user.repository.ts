03fadf9c2f8b9b4f291b8263fc4052bc3c167144
Matías Giménez <meganiunmati@hotmail.com>
2021-09-12 04:41:11 -0300

    add user repository implementation

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,10 +1,27 @@
 import { AbstractRepository, EntityRepository } from "typeorm";
 import { User } from "../models/User";
+import { IUserRepository } from "./interfaces/IUserRepository";
 
 @EntityRepository(User)
-export class UserRepository extends AbstractRepository<User> {
-	async createAndSave(user: User): Promise<User> {
-		return await this.manager.save(user);
+export class UserRepository
+	extends AbstractRepository<User>
+	implements IUserRepository
+{
+	exists(t: User): Promise<boolean> {
+		throw new Error("Method not implemented.");
+	}
+
+	delete(t: User): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
+
+	async save(t: User): Promise<any> {
+		try {
+			return await this.manager.save(t);
+		} catch (error) {
+			console.log(error);
+			throw new Error(error);
+		}
 	}
 
 	async findByUsername(username: string): Promise<User | undefined> {
