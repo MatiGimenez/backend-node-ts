a8df0b9287af22952506ffae1be375324c84ebf7
Matias Gimenez <u631830@T0003036506.fibertel.com.ar>
2021-09-12 00:55:12 -0300

    add config folder with env vars

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+import "dotenv/config"
+
+export const CONFIG = {
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+}
