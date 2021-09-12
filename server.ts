7a405b0cc982dfaea6efa33df38cabcac23a3587
Matias Gimenez <u631830@T0003036506.fibertel.com.ar>
2021-09-12 00:56:21 -0300

    modifiy server to call db connection and server setup

--- server.ts
+++ server.ts
@@ -1,7 +1,20 @@
 import "reflect-metadata";
 import app from "./src/app";
+import connection from './src/database'
+
 const PORT: number = 3000;
 
-app.listen(PORT, (): void => {
-  console.log(`server is listening on ${PORT}`);
-});
+const setUpServer = async () : Promise<void> => {
+  try {
+    await Promise.all([connection, app.listen(PORT)])
+    
+    console.log(
+      `Server now running on localhost:${PORT}`,
+      `Connected to database successfully at ${process.env.DB_NAME}`
+    )
+  } catch (error) {
+    console.error(`Couldn't start the app: ${error}`)
+  }
+}
+
+setUpServer()
