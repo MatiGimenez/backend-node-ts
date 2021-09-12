bd765644d397079b6c1a03b811b7275485118cfd
Matías Giménez <meganiunmati@hotmail.com>
2021-09-12 04:19:32 -0300

    wip auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
 import { UserLogin } from "../models/dtos/user";
+import { AuthenticationService } from "../services";
 
-export const login = (req: Request, res: Response): void => {
-  const { username, password }: UserLogin = req.body;
-  //validar datos
-};
+export class AuthenticationController {
+	authService: AuthenticationService;
+
+	constructor() {
+		this.authService = new AuthenticationService();
+	}
+
+	public async login(req: Request, res: Response): Promise<any> {
+		const { username, password }: UserLogin = req.body;
+		//TODO: validar datos
+		const response: any = await this.authService.login({ username, password });
+		return res.status(201).end();
+	}
+}
