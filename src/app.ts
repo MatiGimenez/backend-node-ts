7a3c06f7028d17451590f1b3ed4650f7a847b366
Matías Giménez <meganiunmati@hotmail.com>
2021-09-16 02:46:07 -0300

    add auth router to app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application } from "express";
+import AuthRoutes from "./routes/auth.routes";
 
 const app: Application = express();
 
+app.use(express.json());
+
+app.use("/", AuthRoutes.router);
 app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
+	res.send("The sedulous hyena ate the antelope!");
 });
 
 export default app;
