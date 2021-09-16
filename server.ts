52c016527b7f55be6ec54cb79f1878c8aa897d7c
Matías Giménez <meganiunmati@hotmail.com>
2021-09-16 02:44:17 -0300

    fix way of initialization of db and app

--- server.ts
+++ server.ts
@@ -1,21 +1,20 @@
 import "reflect-metadata";
-import app from "./src/app";
 import { CONFIG } from "./src/config";
 import connection from "./src/database";
 
 const PORT: number = 3000;
 
 const setUpServer = async (): Promise<void> => {
-	try {
-		await Promise.all([connection, app.listen(PORT)]);
-
-		console.log(
-			`Server now running on localhost:${PORT}`,
-			`Connected to database successfully at ${CONFIG.DB_HOST}`
-		);
-	} catch (error) {
-		console.error(`Couldn't start the app: ${error}`);
-	}
+	connection
+		.then(() => {
+			console.log(`Connected to database successfully at ${CONFIG.DB_HOST}`);
+			import("./src/app").then((module) =>
+				module.default.listen(PORT, () => {
+					console.log(`Server now running on localhost:${PORT}`);
+				})
+			);
+		})
+		.catch((error) => console.error(`Couldn't start the app: ${error}`));
 };
 
 setUpServer();
