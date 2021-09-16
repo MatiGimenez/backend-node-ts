942348fb0fb9732f5b7f8128ef5da25b4d39ea1d
Matias Gimenez <u631830@T0003036506.fibertel.com.ar>
2021-09-16 01:32:54 -0300

    fix port env var

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 export const CONFIG = {
 	DB_HOST: process.env.DB_HOST,
 	DB_NAME: process.env.DB_NAME,
-	DB_PORT: process.env.DB_PORT,
+	DB_PORT: parseInt(process.env.DB_PORT),
 	DB_USER: process.env.DB_USER,
 	DB_PASS: process.env.DB_PASS
 };
