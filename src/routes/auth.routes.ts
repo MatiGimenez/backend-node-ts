ffed92160963ff643f9476b8f4981ecd6845f423
Matías Giménez <meganiunmati@hotmail.com>
2021-09-16 02:45:42 -0300

    add auth router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,22 @@
 import { Router } from "express";
-import { AuthenticationController } from "../controllers/auth.controller";
+import { AuthenticationController } from "../controllers";
 
-const router: Router = Router();
+class AuthRouter {
+	public router: Router;
+	private authController: AuthenticationController;
 
-router.post("login", AuthenticationController);
+	constructor() {
+		this.router = Router();
+		this.authController = new AuthenticationController();
+
+		this.initializeRoutes();
+	}
+
+	private initializeRoutes() {
+		this.router
+			.post("/login", this.authController.login)
+			.post("/register", this.authController.register);
+	}
+}
+
+export default new AuthRouter();
