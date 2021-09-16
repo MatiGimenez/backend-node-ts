f09b07a6466c23786e428417aadd219d07c19616
Matias Gimenez <u631830@T0003036506.fibertel.com.ar>
2021-09-16 01:36:35 -0300

    wip controller layer register flow

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import { UserDTO } from "./../models/dtos/user";
 import { Request, Response } from "express";
 import { UserLogin } from "../models/dtos/user";
 import { AuthenticationService } from "../services";
@@ -14,5 +15,17 @@ export class AuthenticationController {
 		//TODO: validar datos
 		const response: any = await this.authService.login({ username, password });
 		return res.status(201).end();
+	}
+
+	public async register(req: Request, res: Response): Promise<any> {
+		const { username, password, birthDate, email }: UserDTO = req.body;
+		//TODO: validar datos
+		const response: any = await this.authService.register({
+			username,
+			password,
+			birthDate,
+			email
+		});
+		return res.status(200).end();
 	}
 }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { AuthenticationController } from "../controllers/auth.controller";
 
 const router: Router = Router();
+
+router.post("login", AuthenticationController);
