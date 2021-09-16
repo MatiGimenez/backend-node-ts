184c76709c82f5049a92ebb5ab6ad5278b7653d9
Matías Giménez <meganiunmati@hotmail.com>
2021-09-16 02:45:16 -0300

    fix bind methods to pass this context

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,16 +4,28 @@ import { UserLogin } from "../models/dtos/user";
 import { AuthenticationService } from "../services";
 
 export class AuthenticationController {
-	authService: AuthenticationService;
+	private authService: AuthenticationService;
 
 	constructor() {
 		this.authService = new AuthenticationService();
+
+		this.login = this.login.bind(this);
+		this.register = this.register.bind(this);
 	}
 
 	public async login(req: Request, res: Response): Promise<any> {
 		const { username, password }: UserLogin = req.body;
 		//TODO: validar datos
-		const response: any = await this.authService.login({ username, password });
+		try {
+			const response: any = await this.authService.login({
+				username,
+				password
+			});
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+		console.log("HERE");
 		return res.status(201).end();
 	}
 
